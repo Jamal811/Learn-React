ec8be3342a97ff89be7b3c2e3d876639ef63d163
Jamal811 <jamalshah811@gmail.com>
2023-03-28 05:30:05 -0700

    events

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import Parent from "./components/Parent";
 import Profile from "./components/props/Profile";
 import ParentProduct from "./components/props/ParentProduct";
 import State from "./components/State";
+import EventListner from "./components/Events/EventListner";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <ParentProduct />
-      <State />
+      {/* <Navbar /> */}
+      <EventListner />
+      {/* <ParentProduct />
+      <State /> */}
       {/* <Profile /> */}
       {/* <ClickButton />
       <Parent /> */}
