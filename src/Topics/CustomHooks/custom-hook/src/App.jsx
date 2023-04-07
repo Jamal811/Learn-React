b2823c52478600f8fb889ca9984a5a4aeda67bdf
Jamalshah <jimisk811@gmail.com>
2023-04-07 15:41:32 -0700

    custom hook

---
+++ src/Topics/CustomHooks/custom-hook/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useToggle } from "./useToggle";
+function App() {
+  // const [isVisible, toggle] = useToggle();
+  // const { state, toggle } = useToggle();
+  const { state: isVisible, toggle } = useToggle();
+  return (
+    <div className="App">
+      <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
+      <div>{isVisible && <h1>Hidden Text</h1>}</div>
+    </div>
+  );
+}
+
+export default App;
+
+//before custom hook code
+// import { useState } from "react";
+// function App() {
+//   const [isVisible, setIsVisible] = useState(false);
+//   return (
+//     <div className="App">
+//       <button onClick={() => setIsVisible((prev) => !prev)}>
+//         {isVisible ? "Hide" : "Show"}
+//       </button>
+//       {isVisible && <h1>Hidden Text</h1>}
+//     </div>
+//   );
+// }
+
+// export default App;
