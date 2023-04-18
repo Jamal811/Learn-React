91be259c770f9c2430e11e21d5e01ca899a1e916
Jamalshah <jimisk811@gmail.com>
2023-04-18 10:50:56 -0700

    styled components

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/GlobalStyles";
+import Card from "./components/Card";
+import content from "./content";
+import Footer from "./components/Footer";
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+  mobile: "768px",
+};
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <Container>
+        {content.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
+      </Container>
+      <Footer />
+    </ThemeProvider>
+  );
+}
+
+export default App;
